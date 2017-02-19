789a2bd1844407eb27abd4a724665ed5e51ac9e1
RetroSpecter <InsertNameHere4897@gmail.com>
2017-02-19 15:45:47 -0800

    Comparison bar

--- MariaHealth/detailGet.php
+++ MariaHealth/detailGet.php
@@ -6,12 +6,10 @@
 		header("Content-type: application/json");
 		print json_encode(getPlanDetails($title));
 	}
-
+	
 	function getPlanDetails($title){
 		$file = file_get_contents("testGroups.txt");
 		$file_contents = explode("|",$file);
-		
-		
 		return $file_contents;
 	}
 ?>
